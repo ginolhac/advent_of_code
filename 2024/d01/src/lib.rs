00bdf1ffbd76cfb88a87a22998466488dd7562d1
Aurelien Ginolhac <ginolhac@mailbox.org>
2025-10-20 11:11:14 +0200

    d01 part2

--- 2024/d01/src/lib.rs
+++ 2024/d01/src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::collections::HashMap;
 
 pub fn read_pairs(path: &str) -> io::Result<Vec<(i64, i64)>> {
     let f = File::open(path)?;
@@ -24,7 +25,7 @@ pub fn read_pairs(path: &str) -> io::Result<Vec<(i64, i64)>> {
 }
 
 #[test]
-fn d01() {
+fn d01_part1() {
     let pairs = read_pairs("../input/01").expect("read input");
    
     for (a, b) in &pairs {
@@ -45,3 +46,27 @@ fn d01() {
     .sum();
     println!("sum diff = {:?}", sum_abs_diff);
 }
+
+#[test]
+fn d01_part2() {
+    let pairs = read_pairs("../input/01").expect("read input");
+    let mut map: HashMap<i64, Vec<i64>> = HashMap::new();
+    // initialize to zeroes with keys from a values
+    for (a, _) in &pairs {
+        map.entry(*a).or_default().push(0);
+    }
+    for (_, b) in &pairs {
+        if map.contains_key(b) {
+            map.entry(*b).or_default().push(1);
+        }
+    }
+
+    println!("map len = {}", map.len());
+    let mut sum: i64 = 0;
+    for (k, v) in &map {
+        sum += k * v.iter().sum::<i64>();
+        //println!("{} -> {:?} ({})", k, v, sum);
+    }
+    println!("final sum = {}", sum);
+}
+
